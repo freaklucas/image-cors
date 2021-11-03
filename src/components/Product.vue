2bbe84dcc9abf4e530a9419cfcf5427c3c008dc5
freaklucas <lucas.oliveira.freak@gmail.com>
2021-11-02 23:54:56 -0300

    home

---
+++ src/components/Product.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="switchableGrid">
+    <div class="container">
+      <div class="bar">
+        <div class="btnHolder">
+          <button class="barActive">
+            <i class="fa fa-th"></i>
+            Grid
+          </button>
+          <button>
+            <i class="fas fa-list">
+            </i>
+            List
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
